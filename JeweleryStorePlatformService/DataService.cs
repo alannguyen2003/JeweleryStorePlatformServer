887903b6ab1355a0d270800a25af2de8747150ec
alannguyen2003 <nguyenho30112003@gmail.com>
2024-07-15 17:34:49 +0700

    add auto migration for next implementation

---
+++ JeweleryStorePlatformService/DataService.cs
@@ -0,0 +1,18 @@
+﻿using JeweleryStorePlatformRepository.Interface;
+using JeweleryStorePlatformService.Interface;
+
+namespace JeweleryStorePlatformService;
+
+public class DataService : IDataService
+{
+    private readonly IDataRepository _dataRepository;
+
+    public DataService(IDataRepository dataRepository)
+    {
+        _dataRepository = dataRepository;
+    }
+    public async Task MigrationAsync()
+    {
+        await _dataRepository.MigrationAsync();
+    }
+}
