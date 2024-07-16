3b399cb2ce3f01bd13392f66d60945ca028afd03
nguyetntmse161788 <nguyetthm02@gmail.com>
2024-07-16 19:12:44 +0700

    Update JeweleryTypeService.cs

--- JeweleryStorePlatformService/JeweleryTypeService.cs
+++ JeweleryStorePlatformService/JeweleryTypeService.cs
@@ -1,6 +1,5 @@
-using JeweleryStorePlatformBusinessObject.Design;
 using JeweleryStorePlatformBusinessObject.Jewelery;
-using JeweleryStorePlatformRepository.Interface;
+using JeweleryStorePlatformDataAccess;
 using JeweleryStorePlatformService.Interface;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -11,15 +10,11 @@ namespace JeweleryStorePlatformService
     public class JeweleryTypeService : IJeweleryTypeService
     {
         private readonly AppDbContext _context;
-    public JeweleryTypeService(IJeweleryTypeRepository jeweleryTypeRepository)
-    {
-        _jeweleryTypeRepository = jeweleryTypeRepository;
-    }
 
-    public async Task<List<JeweleryType>> GetAllJeweleryType()
-    {
-        return await _jeweleryTypeRepository.GetAllJeweleryType();
-    }
+        public JeweleryTypeService(AppDbContext context)
+        {
+            _context = context;
+        }
 
         public async Task<List<JeweleryType>> GetAllJeweleryType()
         {
