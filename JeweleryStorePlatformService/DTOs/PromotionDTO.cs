d9231d97da9a6ef9ea1678f1d3a6038cf4f81beb
Yu282002 <yoshino291@gmail.com>
2024-07-18 16:06:55 +0700

    CRUD Promotion

---
+++ JeweleryStorePlatformService/DTOs/PromotionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeweleryStorePlatformService.DTOs
+{
+    public class PromotionDTO
+    {
+        public string PromotionName { get; set; }
+        public string PromotionContent { get; set; }
+        public int Amount { get; set; }
+        public int Percentage { get; set; }
+    }
+}
---
+++ JeweleryStorePlatformService/PromotionService.cs
@@ -0,0 +1,44 @@
+﻿using JeweleryStorePlatformBusinessObject.Promotion;
+using JeweleryStorePlatformRepository;
+using JeweleryStorePlatformRepository.Interface;
+using JeweleryStorePlatformService.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JeweleryStorePlatformService
+{
+    public class PromotionService : IPromotionService
+    {
+        private readonly IPromotionRepository _promotionRepository;
+
+        public PromotionService(IPromotionRepository promotionRepository)
+        {
+            _promotionRepository = promotionRepository;
+        }
+
+        public async Task<List<Promotion>> GetAllPromotions()
+        {
+            return await _promotionRepository.GetAllPromotions();
+        }
+
+        public async Task AddNewPromotion(Promotion promotion)
+        {
+            await _promotionRepository.AddNewPromotion(promotion);
+        }
+
+        public async Task<Promotion> UpdatePromotion(Promotion promotion)
+        {
+            return await _promotionRepository.UpdatePromotion(promotion);
+        }
+
+        public async Task<bool> DeletePromotion(int promotionId)
+        {
+            return await _promotionRepository.DeletePromotion(promotionId);
+        }
+
+        public async Task<Promotion> GetPromotionById(int promotionId)
+        {
+            return await _promotionRepository.GetPromotionById(promotionId);
+        }
+    }
+}
